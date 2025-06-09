7d347f19b2275162a2e035b54331aed8b2bf4f1e
Mohammed Taysir Alkhatib <medo-11223344@hotmail.com>
2025-06-09 19:31:04 +0300

    Pagintion and Modal For Products Page

--- assets/js/products.js
+++ assets/js/products.js
@@ -48,9 +48,12 @@ function filteringProducts() {
 
 async function getCategoryProducts(category) {
     try {
+        
         const {data} = await axios.get(`https://dummyjson.com/products/category/${category}`);
+        
         const productsContainer = document.querySelector(".products-content");
         createProductsHtml(data.products, productsContainer);
+        document.querySelector(".pagination").innerHTML = "";
     }catch(error) {
         console.log(error);
     }
@@ -58,8 +61,21 @@ async function getCategoryProducts(category) {
 
 async function getProducts() {
     try {
-        const {data} = await axios.get("https://dummyjson.com/products");
+       
+        const params = new URLSearchParams(window.location.search);
+        let page = parseInt(params.get("page")) || 1;
+        
+        const limit = 9;
+        const skip = (page - 1) * limit;
+        const {data} = await axios.get(`https://dummyjson.com/products?limit=${limit}&skip=${skip}`);
         const products = data["products"];
+        const numOfPages = Math.ceil(data.total / limit);
+
+        if(page <= 0 || page > numOfPages)
+            window.location.href = `./products.html?page=1`;
+
+        customPagination(numOfPages, page);
+        document.querySelector(`.pag-${page}`).classList.add("active");
         const productContainer = document.querySelector(".products-content"); 
         createProductsHtml(products, productContainer);
     }catch(error) {
@@ -78,9 +94,10 @@ async function getProducts() {
 function createProductsHtml(products, productContainer) {  
     productContainer.innerHTML =  products.map((product) => 
         `
-            <div data-id = "${product.id}" class = "product main-shadow cursor-pointer p-20 bg-white b-6 d-flex flex-column gap-10 align-center ${product.category} all">
-                <div class = "image center-flex"> 
-                    <img src = "${product.thumbnail}" />
+            <div data-id = "${product.id}" class = "product main-shadow p-20 bg-white b-6 d-flex flex-column gap-10 align-center ${product.category} all">
+                <div class = "image center-flex p-relative"> 
+                    <img src = "${product.thumbnail}" class = "product-img" />
+                    <a class = "btn p-absolute view-details" href = "./product.html?id=${product.id}">View Details</a>
                 </div>
                 <h3 class = "center-flex">${product.title}</h3>
                 <div class = "info row">    
@@ -90,14 +107,101 @@ function createProductsHtml(products, productContainer) {
             </div>
         `
 ).join("");
+    customModal();
+}
 
-    document.querySelectorAll(".product").forEach(product=> {
-        product.addEventListener("click", ()=> {
-            window.location.href = `./product.html?id=${product.getAttribute("data-id")}`;
-        });
+function customModal() {
+    const images = document.querySelectorAll(".product-img");
+    const modal = document.querySelector(".my-modal");
+    const modalImage = document.querySelector(".modal-img");
+    const closeBtn = document.querySelector(".close-btn");
+    const rightBtn = document.querySelector(".right-btn");
+    const leftBtn = document.querySelector(".left-btn");
+    let currentIndex;
+    images.forEach((image, index) => {
+        image.onclick = (e) => {
+            modal.classList.add("show");
+            modalImage.setAttribute("src", e.target.getAttribute("src"));
+            currentIndex = index;
+        }
     });
 
+    closeBtn.onclick = () => {
+        closeModal();
+    }
+    
+    rightBtn.onclick = () => {
+        nextImage();
+    }
+
+    leftBtn.onclick = () => {
+       prevImage();
+    }
+    
+    document.onkeydown = (e)=> {
+        if(e.key === "ArrowRight")
+            nextImage();
+
+        if(e.key === "ArrowLeft")
+            prevImage();
+
+        if(e.key === "Backspace")
+            closeModal();
+    }
+
+    function closeModal() {
+        modal.classList.remove("show");
+    }
+
+    function nextImage() {
+        ++currentIndex;
+        if(currentIndex >= images.length)
+            currentIndex = 0;
+        modalImage.setAttribute("src", images[currentIndex].getAttribute("src"));
+    }
+
+    function prevImage() {
+         --currentIndex;
+        if(currentIndex < 0)
+            currentIndex = 0;
+        modalImage.setAttribute("src", images[currentIndex].getAttribute("src"));
+    }
 }
+
+function customPagination(numOfPages, page) {
+    const itemsVisible = itemsToShow();
+    const startPage = Math.max(1, page - Math.floor(itemsVisible / 2));
+    const endPage = Math.min(numOfPages, startPage + itemsVisible - 1);
+
+    let paginations = `<li class = "center-flex main-shadow"><a href="./products.html?page=${page-1}">&lt;</a></li>`;
+    for(let i = startPage; i <= endPage; i++) {
+        paginations += `<li class = "center-flex main-shadow pag-${i}"><a href="./products.html?page=${i}">${i}</a></li>`;
+    }
+    paginations += `<li class = "center-flex main-shadow"><a href="./products.html?page=${page+1}">&gt;</a></li>`;
+
+    document.querySelector(".pagination").innerHTML = paginations;
+    if(page === 1)
+        preventMove(document.querySelector(".pagination li:first-child"));
+
+    if(page >= numOfPages)
+        preventMove(document.querySelector(".pagination li:last-child"));
+    
+    function preventMove(link) {
+        link.children[0].classList.add("prevented");
+        link.classList.add("cursor-not")
+    }
+
+    function itemsToShow() {
+        if(window.innerWidth  <= 768)
+            return 5;
+        else if(window.innerWidth <= 992) 
+            return 8;
+        else 
+            return 15;
+
+        
+    }
+} 
 
 function getParams() {
     const params = new URLSearchParams(window.location.search);
